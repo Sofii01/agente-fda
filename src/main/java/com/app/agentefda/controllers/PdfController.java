59993d5542ccbebb57c168a597d6b54a93cec991
SofiaBritps <sofiibritos01@gmail.com>
2025-08-05 20:24:53 -0300

    feat: add pdf reading with service and controller

--- src/main/java/com/app/agentefda/controllers/PdfController.java
+++ src/main/java/com/app/agentefda/controllers/PdfController.java
@@ -1,4 +1,30 @@
 package com.app.agentefda.controllers;
 
+import com.app.agentefda.services.PdfService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/pdf")
 public class PdfController {
+    private final PdfService pdfService;
+
+    public PdfController(PdfService pdfService) {
+        this.pdfService = pdfService;
+    }
+    @PostMapping
+    public ResponseEntity<?> leerPdf(@RequestParam("archivo") MultipartFile archivo) {
+        try {
+            String text = pdfService.extraerTextoDePdf(archivo);
+            return ResponseEntity.ok().body(text);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/app/agentefda/services/PdfService.java
+++ src/main/java/com/app/agentefda/services/PdfService.java
@@ -1,4 +1,24 @@
 package com.app.agentefda.services;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
 public class PdfService {
+
+    public String extraerTextoDePdf(MultipartFile archivo) throws IOException {
+        try (PDDocument document = PDDocument.load(archivo.getInputStream())){
+            PDFTextStripper stripper = new PDFTextStripper();
+            String texto = stripper.getText(document);
+            return limpiarRespuesta(texto); //limpiar texto
+        }
+    }
+
+    private String limpiarRespuesta(String texto) {
+        return texto.replaceAll("\\s+", " ");
+    }
 }
